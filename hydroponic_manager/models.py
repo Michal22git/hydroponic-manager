94ee8fde2f756beafc0aae32552e3c2c57c999a4
micha <michalfiszer15@protonmail.com>
2025-02-07 19:40:14 +0100

    added models

--- hydroponic_manager/models.py
+++ hydroponic_manager/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class HydroponicSystem(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Measurement(models.Model):
+    system = models.ForeignKey(HydroponicSystem, on_delete=models.CASCADE)
+    ph = models.FloatField(
+        validators=[
+            MinValueValidator(0.0),
+            MaxValueValidator(14.0),
+        ]
+    )
+    tds = models.PositiveIntegerField()
+    water_temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
