589145b23c7abc5a551f3f0b7f7bbaef73f1eaa8
micha <michalfiszer15@protonmail.com>
2025-02-07 20:52:50 +0100

    configured django admin panel

--- hydroponic_manager/admin.py
+++ hydroponic_manager/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import HydroponicSystem, Measurement
+
+
+@admin.register(HydroponicSystem)
+class HydroponicSystemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'created_at')
+    list_filter = ('created_at', 'owner')
+    search_fields = ('name', 'description')
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('system', 'ph', 'water_temperature', 'tds', 'created_at')
+    list_filter = ('created_at', 'system')
+    search_fields = ('system__name',)
